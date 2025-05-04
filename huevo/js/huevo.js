946844b6ed5a9798d2a91b4dbb3715ce4f788ec9
Alba <aalbasaanz@gmail.com>
2025-05-04 23:38:28 +0200

    Update Infografia Huevo

--- huevo/js/huevo.js
+++ huevo/js/huevo.js
@@ -4,4 +4,24 @@ function inicio(){
     $("#pestannas").tabs({
         event:"click"
     });
+    
+    $('#cascara').on('click', function() {
+        ocultarTodo();
+        $('#infoCascara').show();
+    });
+
+    $('#clara').on('click', function() {
+        ocultarTodo();
+        $('#infoClara').show();
+    });
+
+    $('#yema').on('click', function() {
+        ocultarTodo();
+        $('#infoYema').show();
+    });
 }
+
+function ocultarTodo() {
+    $('.infoParte').hide();
+}
+
