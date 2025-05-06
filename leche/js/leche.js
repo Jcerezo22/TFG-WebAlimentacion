5e86880cbaa8d79a74720bae45014b5f47fbbecb
Alba <aalbasaanz@gmail.com>
2025-05-06 21:23:10 +0200

    Prueba Cargar Receta

--- leche/js/leche.js
+++ leche/js/leche.js
@@ -4,4 +4,11 @@ function inicio(){
     $("#pestannas").tabs({
         event:"click"
     });
+
+    $('.cargar').on('click', function(e) {
+        e.preventDefault();
+        const url = $(this).data('url');
+        $('#recetasTab').load(url); // Cargar contenido externo en esa pestaña
+      });
 }
+
