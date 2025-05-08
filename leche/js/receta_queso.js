77b7b240844637d0dae6acccc2cca1397eff23db
Alba <aalbasaanz@gmail.com>
2025-05-08 15:01:03 +0200

    Update Barraa Ph receta queso

--- leche/js/receta_queso.js
+++ leche/js/receta_queso.js
@@ -4,14 +4,20 @@ else if (document.attachEvent)
 	window.attachEvent("onload", inicio);
 
 function inicio(){
-    let contenedor = document.querySelector('.contenedorOpciones');
+    let contenedor = document.querySelector(".contenedorOpciones");
+    let barraPh = document.getElementById("phRango")
 
     if (document.addEventListener) {
         contenedor.addEventListener("click", selecionarTipoLeche);
+        barraPh.addEventListener("input", actualizarValorPh);
     } else if (document.attachEvent) {
         contenedor.attachEvent("onclick", selecionarTipoLeche);
+        barraPh.attachEvent("oninput", actualizarValorPh);
     }
+
+    barraPh.dispatchEvent(new Event('input'));
 }
+
 
 function selecionarTipoLeche(event){
 
@@ -33,4 +39,21 @@ function selecionarTipoLeche(event){
         
         console.log(tipoLeche);
     }
+}
+
+function actualizarValorPh(event){
+    let barra = event.target;
+
+    let valor = parseFloat(barra.value);
+    let min = parseFloat(barra.min);
+    let max = parseFloat(barra.max);
+
+    let porcentaje = ((valor - min) / (max - min)) * 100;
+
+    document.getElementById("phValorMov").textContent = valor.toFixed(1);
+
+    let colorIzquierda = "#77b4d8"; // verde
+    let colorDerecha = "#ddd";      // gris
+
+    barra.style.background = `linear-gradient(to right, ${colorIzquierda} 0%, ${colorIzquierda} ${porcentaje}%, ${colorDerecha} ${porcentaje}%, ${colorDerecha} 100%)`;
 }
