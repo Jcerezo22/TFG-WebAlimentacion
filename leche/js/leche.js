37d6e843ffccab896154167d09a6e2459b0d86ec
Alba <aalbasaanz@gmail.com>
2025-05-21 13:42:37 +0200

    Update Resize Mapa

--- leche/js/leche.js
+++ leche/js/leche.js
@@ -9,6 +9,17 @@ function inicio(){
         e.preventDefault();
         const url = $(this).data('url');
         $('#recetasTab').load(url); // Cargar contenido externo en esa pestaña
-      });
+    });
+
+
+    let imagenMapa = $('#imagenMapa');
+
+    imagenMapa.on('load', function () {
+        $('map').imageMapResize();
+    });
+
+    if (imagenMapa[0].complete) {
+        $('map').imageMapResize();
+    }
 }
 
