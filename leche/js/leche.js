a4e91f572911dc1eb946471adff00cef0024cab0
Jcerezo22 <javiercerezocerrada@gmail.com>
2025-05-21 14:43:36 +0200

    Update inicio apartado paramfq

--- leche/js/leche.js
+++ leche/js/leche.js
@@ -9,6 +9,25 @@ function inicio(){
         e.preventDefault();
         const url = $(this).data('url');
         $('#recetasTab').load(url); // Cargar contenido externo en esa pestaña
-      });
+    });
+
+    $('.acidez').on('click', function() {
+        ocultarTodo();
+        $('#infoAcidez').show();
+    });
+
+    $('#clara').on('click', function() {
+        ocultarTodo();
+        $('#infoClara').show();
+    });
+
+    $('#yema').on('click', function() {
+        ocultarTodo();
+        $('#infoYema').show();
+    });
+}
+
+function ocultarTodo() {
+    $('.infoParte').hide();
 }
 
