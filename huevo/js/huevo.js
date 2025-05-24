da9e3528d4180f30bc7f0f8edd19ea660f21afd5
Alba <aalbasaanz@gmail.com>
2025-05-24 20:15:34 +0200

    Update Simulador Huevo

--- huevo/js/huevo.js
+++ huevo/js/huevo.js
@@ -19,9 +19,40 @@ function inicio(){
         ocultarTodo();
         $('#infoYema').show();
     });
+
+    $("#botonFrescura").on("click", calcularFrescura);
 }
 
 function ocultarTodo() {
     $('.infoParte').hide();
+}
+
+function calcularFrescura(event){
+    event.preventDefault();
+
+    let tipoFecha = $('input[name="tipoFecha"]:checked').val(); ;
+    let fechaStr = $('#fechaInput').val();
+
+    if (fechaStr) {
+      let fecha = new Date(fechaStr);
+      let fechaActual = new Date();
+
+      if(tipoFecha == "fcp"){
+        fecha.setDate(fecha.getDate() - 28);
+      }
+
+      let diferenciaFechas = fechaActual - fecha;
+      let diasPasados = Math.floor(diferenciaFechas / (1000 * 60 * 60 * 24));
+      $('#resultadoSimulador').removeClass('estadoInicial').addClass('estadoFinal');
+      $('#mensajeResultado').text('📅 Días desde la puesta: '+diasPasados);
+
+    } else {
+        $('#mensajeResultado').text('Por favor selecciona una fecha.');
+    }
+
+    
+
+    // $('#mensajeResultado').text('✅ El huevo está fresco medio (🟡)');
+    // $('#imagenResultado').attr('src', 'huevo-fresco-medio.png');
 }
 
